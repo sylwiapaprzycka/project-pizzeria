79a4eaf03c4b946d020d9253069398b818b94a4a
Sylwia Paprzycka <sylwia.paprzycka@gmail.com>
2020-07-20 10:02:32 +0200

    add remove(cartProduct) event

--- dist/js/script.js
+++ dist/js/script.js
@@ -374,7 +374,19 @@
       thisCart.dom.productList.addEventListener('updated', function() {
         thisCart.update();
       });
+      thisCart.dom.productList.addEventListener('remove', function() {
+        thisCart.remove(event.detail.cartProduct);
+      });
     }  
+
+    remove(cartProduct) {
+      const thisCart = this;
+      const index = thisCart.products.indexOf(cartProduct);
+      thisCart.products.splice(index);
+      cartProduct.dom.wrapper.remove();
+
+      thisCart.update();
+    }
     
     add(menuProduct) {
       const thisCart = this;
@@ -389,6 +401,7 @@
       // console.log('thisCart.products', thisCart.products);
       thisCart.update();
     }
+
     update() {
       const thisCart = this;
       thisCart.totalNumber = 0;
@@ -422,6 +435,7 @@
       thisCartProduct.params = JSON.parse(JSON.stringify(menuProduct.params));
       thisCartProduct.getElements(element);
       thisCartProduct.initAmountWidget();
+      thisCartProduct.initActions();
 
       // console.log('new CartProduct', thisCartProduct);
       // console.log('productData', menuProduct);
@@ -436,6 +450,7 @@
       thisCartProduct.dom.edit = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.edit);
       thisCartProduct.dom.remove = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.remove);
     }
+
     initAmountWidget() {
       const thisCartProduct = this;
 
@@ -444,6 +459,31 @@
         thisCartProduct.amount = thisCartProduct.amountWidget.value;
         thisCartProduct.price = thisCartProduct.priceSingle * thisCartProduct.amount;
         thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
+      });
+    }
+
+    remove() {
+      const thisCartProduct = this;
+      
+      const event = new CustomEvent('remove', {
+        bubbles: true,
+        detail: {
+          cartProduct: thisCartProduct,
+        }
+      });
+      thisCartProduct.dom.wrapper.dispatchEvent(event);
+
+      console.log('remove', event);
+    }
+
+    initActions() {
+      const thisCartProduct = this;
+      thisCartProduct.dom.edit.addEventListener('click', function(event) {
+        event.preventDefault();
+      });
+      thisCartProduct.dom.remove.addEventListener('click', function(event) {
+        event.preventDefault();
+        thisCartProduct.remove();
       });
     }
   }
